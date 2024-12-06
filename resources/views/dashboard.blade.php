55d826cb5b7634cddd866b29f444a48c98855b85
isaquesene <isaqueconcursos26@gmail.com>
2024-12-05 22:22:07 -0300

    fix: melhorias

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-6 rounded-lg shadow-lg text-center">
+                    <h2 class="text-2xl font-bold">{{ __("Olá, Seja bem-vindo ao BULKSHIP!") }}</h2>
+                    <p class="mt-2 text-lg">{{ __("Estamos felizes em tê-lo conosco. Vamos começar!") }}</p>
                 </div>
             </div>
         </div>
