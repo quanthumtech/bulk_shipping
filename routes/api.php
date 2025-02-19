d50d6d5d3d7f7691a0404feb4cb75a444b094d11
isaquesene <isaqueconcursos26@gmail.com>
2025-02-19 20:05:47 -0300

    fix: api comentada

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\WebhookZohoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/webhook-bulkship', [WebhookZohoController::class, 'createFromWebhook']);
+//Route::post('/webhook-bulkship', [WebhookZohoController::class, 'createFromWebhook']);
