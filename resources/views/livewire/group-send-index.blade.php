62194d854eb6b3bceb9d7686eb5fdf1a952445d4
isaquesene <isaqueconcursos26@gmail.com>
2025-04-04 16:27:48 -0300

    fix: asjustes de modal

--- resources/views/livewire/group-send-index.blade.php
+++ resources/views/livewire/group-send-index.blade.php
@@ -25,7 +25,7 @@
         separator
         with-close-button
         close-on-escape
-        class="w-11/12 lg:w-1/2"
+        class="w-11/13 lg:w-1/2"
         right
     >
         <x-mary-form wire:submit="save">
