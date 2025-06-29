0ac7f356acb4d0573cca503fe397db6fb6910461
isaquesene <isaqueconcursos26@gmail.com>
2025-06-29 20:33:16 -0300

    fix: config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             'api/webhook-bulkship',
-        ]);
+            'api/chatwoot_webhook',
+            'webhook-bulkship-syncflow',
+            'api/webhook-bulkship-syncflow'
+        ])->append(StartSession::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
